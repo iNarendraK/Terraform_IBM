cf10461f6192daf434ba377c40afeac8373dc755
iNarendraK <49773554+iNarendraK@users.noreply.github.com>
2022-12-06 20:29:32 +0530

    Update provider.tf

--- modules/kube-cluster-openshift/provider.tf
+++ modules/kube-cluster-openshift/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    openshift = {
+      source = "llomgui/openshift"
+      version = "1.1.0"
+    }
+  }
+}
+
 provider "openshift" {
   load_config_file = "false"
 
